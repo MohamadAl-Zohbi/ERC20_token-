a7dfdaad00caa9f7b58b0b413d7cfecd957fa06c
MohamadAl-Zohbi <92718300+MohamadAl-Zohbi@users.noreply.github.com>
2023-01-17 20:37:21 +0200

    Create erc20.sol

---
+++ erc20.sol
@@ -0,0 +1,49 @@
+``` solidity
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor() public {
+        name = "My Token";
+        symbol = "MYT";
+        decimals = 18;
+        totalSupply = 100000000e18;
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 value) public {
+        require(balanceOf[msg.sender] >= value);
+        require(to != address(0));
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+    }
+
+    function approve(address spender, uint256 value) public returns (bool) {
+        require(balanceOf[msg.sender] >= value);
+        require(spender != address(0));
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        require(balanceOf[from] >= value);
+        require(to != address(0));
+        require(balanceOf[from] >= value && allowed[from][msg.sender] >= value);
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint256 remaining) {
+        return allowed[tokenOwner][spender];
+    }
+}
+```
